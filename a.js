f00d59161e101dd50e206a45495339e34ea0c8b7
Masayuki Sato <mijabi@gmail.com>
2015-10-10 22:46:29 +0900

    add function that allows select elements simply

--- a.js
+++ a.js
@@ -1,39 +1,57 @@
 (function($d){
 
 	var mija = {
+
+		// add multiple styles by Object
 		giveClass: function($elem, css){
 			for(var i in css) {
 				$elem.style[i] = css[i];
 			}
 		},
+
+		// load JSON file asynchronously
 		loadJson: function(url, success, error){
 			var xmlhttp = new XMLHttpRequest();
 			xmlhttp.open('GET' , url , true);
 			xmlhttp.onreadystatechange = function() {
 			    if(4 != xmlhttp.readyState) return;
 			    if(200 != xmlhttp.status) return;
-				success(JSON.parse(xmlhttp.responseText));
+				success(JSON.parse(xmlhttp.responseText), xmlhttp.responseText);
 			};
 			xmlhttp.send();
-		}
-	}
+		},
 
-	// load json
-	mija.loadJson('a.json', function(json){
-			alert(json.lawson.a);
-		}, function(str){
-			// occured error
-		});
+		// get HTML Elements simply
+		bi: function(str){
+			switch(str.charAt(0)) {
+				case '#' :
+					return $d.getElementById(str.substring(1));
+					break;
+				case '.' :
+					// if there's only 1 class element, respond it. if they are more than 2, respond Array
+					var $classes = $d.getElementsByClassName(str.substring(1));
+					return $classes.length == 1 ? $classes[0] : $classes;
+					break;
+				default :
+					// if there's only 1 tag, respond it. if they are more than 2, respond Array
+					var $tags = $d.getElementsByTagName(str);
+					return $tags.length == 1 ? $classes[0] : $classes;
+					break;
+			}
+
+		}
+
+	}
 
 
 	//
-	var $idElem = $d.getElementById('h1');
+	var $idElem = mija.bi('#h1');
 	$idElem.addEventListener('click', function(){
 		alert('hoge');
 	});
 
 	// 
-	var $classElem2 = $d.getElementsByClassName('h2')[0];
+	var $classElem2 = mija.bi('.h2');
 	$classElem2.addEventListener('mouseenter', function(e){
 		var $e = e.currentTarget;
 		mija.giveClass($e, {
@@ -56,9 +74,21 @@
 	});
 
 	//
-	var $classElem = $d.getElementsByClassName('button')[0];
+	var $classElem = mija.bi('.button');
 	$classElem.addEventListener('click', function(){
 		alert('hoge');
+	});
+
+	// load json
+	mija.bi('.load-json').addEventListener('click', function(){
+		mija.loadJson('a.json',
+			function(json){
+				var $targ = mija.bi('.textarea');
+				// $targ.innerHTML = arguments[1];
+				$targ.innerHTML = json.lawson.a;
+			}, function(str){
+				// occured error
+			});
 	});
 
 
